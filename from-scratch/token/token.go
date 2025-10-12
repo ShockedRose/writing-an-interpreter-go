c9c24ac1289ba673258d152417eb5f6c1a074653
shockedRose <miguelj0916@gmail.com>
2025-10-12 11:31:42 -0500

    initial commit, from scratch up to 1.3

---
+++ from-scratch/token/token.go
@@ -0,0 +1,48 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+
+	IDENT = "IDENT" // add, foobar, x... etc.
+	INT   = "INT"   // 1231321
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
